ea16152ac0e8eecd9d08828519bd1c501bb365bb
focus.guo <focus.guo@happyeasygo.com>
2022-08-30 17:34:57 +0800

    02｜TDD演示（2）：识别坏味道与代码重构

--- src/main/java/args/Args.java
+++ src/main/java/args/Args.java
@@ -4,21 +4,28 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
- * @Author: focus.guo
- * @Date: 2022/8/27 19:37
- * @Description:
+ * @Author: focus.guo @Date: 2022/8/27 19:37 @Description:
  */
 public class Args {
+
+    private static final Map<Class<?>, OptionParser> PARSERS =
+            Map.of(
+                    boolean.class, new BooleanOptionParser(),
+                    int.class, new SingleValueOptionParser<>(Integer::parseInt),
+                    String.class, new SingleValueOptionParser<>(String::valueOf));
 
     public static <T> T parse(Class<T> optionsClass, String... args) {
         Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
         try {
             List<String> arguments = Arrays.stream(args).toList();
-            Object[] values = Arrays.stream(constructor.getParameters())
-                    .map(it -> parseOption(arguments, it))
-                    .toArray();
+
+            Object[] values =
+                    Arrays.stream(constructor.getParameters())
+                            .map(it -> parseOption(arguments, it))
+                            .toArray();
 
             return (T) constructor.newInstance(values);
         } catch (Exception e) {
@@ -27,20 +34,9 @@ public class Args {
     }
 
     private static Object parseOption(List<String> arguments, Parameter parameter) {
-        Object value = null;
         Option option = parameter.getAnnotation(Option.class);
-
-        if (parameter.getType() == boolean.class) {
-            value = arguments.contains("-" + option.value());
-        }
-        if (parameter.getType() == int.class) {
-            int index = arguments.indexOf("-" + option.value());
-            value = Integer.parseInt(arguments.get(index + 1));
-        }
-        if (parameter.getType() == String.class) {
-            int index = arguments.indexOf("-" + option.value());
-            value = arguments.get(index + 1);
-        }
-        return value;
+        Class<?> parameterType = parameter.getType();
+        OptionParser parser = PARSERS.get(parameterType);
+        return parser.parse(arguments, option);
     }
 }
---
+++ src/main/java/args/SingleValueOptionParser.java
@@ -0,0 +1,20 @@
+package args;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class SingleValueOptionParser<T> implements OptionParser {
+    Function<String, T> valueParser;
+
+    public SingleValueOptionParser(Function<String, T> valueParser) {
+        this.valueParser = valueParser;
+    }
+
+    @Override
+    public T parse(List<String> arguments, Option option) {
+        int index = arguments.indexOf("-" + option.value());
+        String value = arguments.get(index + 1);
+        return valueParser.apply(value);
+    }
+
+}
