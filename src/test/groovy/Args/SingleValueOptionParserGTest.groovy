6065993ba9a76c56aaae1ecfcc9455dd588e457e
focus.guo <go_focus@live.cn>
2022-08-31 14:38:52 +0800

    引入 spock, groovy

---
+++ src/test/groovy/Args/SingleValueOptionParserGTest.groovy
@@ -0,0 +1,17 @@
+package Args
+
+import spock.lang.Specification
+
+/**
+ * @Author: focus.guo
+ * @Date: 2022/8/31 13:56
+ * @Description:
+ */
+class SingleValueOptionParserGTest extends Specification {
+
+    def "test"() {
+        expect:
+        1 == 1
+    }
+
+}
