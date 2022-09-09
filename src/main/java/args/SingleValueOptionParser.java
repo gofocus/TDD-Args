564d5d5a1a76fca5213953788d6c2cfd30494dce
focus.guo <go_focus@live.cn>
2022-09-09 17:04:41 +0800

    04｜TDD演示（4）：实现对于列表参数的支持 | 不易察觉的坏味道 | video-1

--- src/main/java/args/SingleValueOptionParser.java
+++ src/main/java/args/SingleValueOptionParser.java
@@ -2,6 +2,7 @@ package args;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class SingleValueOptionParser<T> implements OptionParser<T> {
     Function<String, T> valueParser;
@@ -15,23 +16,30 @@ public class SingleValueOptionParser<T> implements OptionParser<T> {
     @Override
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
-
-        boolean optionExists = index != -1;
-        boolean noParam = index + 1 == arguments.size() || arguments.get(index + 1).startsWith("-");
-
-        if (!optionExists){
+        if (index == -1) {
             return defaultValue;
         }
 
-        if (noParam) {
+        List<String> values = values(arguments, index);
+
+        if (values.size() < 1) {
             throw new InsufficientArgumentsException(option.value());
         }
-
-        if (index + 2 < arguments.size() && !arguments.get(index + 2).startsWith("-")) {
+        if (values.size() > 1) {
             throw new TooManyArgumentsException(option.value());
         }
 
-        return valueParser.apply(arguments.get(index + 1));
+        String value = values.get(0);
+
+        return valueParser.apply(value);
     }
 
+    private static List<String> values(List<String> arguments, int index) {
+        int followingFlag =
+                IntStream.range(index + 1, arguments.size())
+                        .filter(it -> arguments.get(it).startsWith("-"))
+                        .findFirst()
+                        .orElse(arguments.size());
+        return arguments.subList(index + 1, followingFlag);
+    }
 }
